c69c3ee54cf1adf75e59f7c021910c5be5190429
tgarwis <2010631170035@student.unsika.ac.id>
2024-09-03 15:36:02 -0700

    Menambah isi file Route

--- Praktikum-1/study-laravel-11/routes/web.php
+++ Praktikum-1/study-laravel-11/routes/web.php
@@ -2,6 +2,50 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Rute GET sederhana
+
+Route ::get('/hello', function () {
+    return 'Hello, World!';
+});
+
+// //Rute dengan parameter
+// Route ::get('/user/{id}', function ($id) {
+//     return "User ID: " . $id;
+// });
+
+//Rute dengan parameter optional
+Route ::get('/user/{name?}', function($name = 'Guest'){
+    return "Selamat datang, ". $name;});
+
+//Rute dengan nama
+Route::get('/profile', function(){
+    return "Halaman Profile";
+})->name('profile');
+
+//Menggunakan rute bernama untuk pengalihan
+Route::get('/redirect-to-profile', function(){
+    return redirect()->route('profile');
+});
+
+//Rute Group
+Route::prefix('admin')->group(function(){
+    Route::get('/dashboard', function(){
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/profile', function(){
+        return 'Admin Profile';
+    });
+});
+
+//Rute Middleware
+Route::get('/dashboard', function(){
+    return 'Welcome to dashboard';
+})->middleware('auth');
+
+//Rute Resource
+Route::resource('Post', 'PostController');
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
