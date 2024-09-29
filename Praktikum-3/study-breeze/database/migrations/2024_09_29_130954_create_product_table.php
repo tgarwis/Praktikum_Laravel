c6857fde98280ac114e78e7e35e30ecffd2cf2b0
tgarwis <2010631170035@student.unsika.ac.id>
2024-09-29 06:43:23 -0700

    Membuat migrasi table product, menginstall Breeze, dan membuat akun demo menggunakan seeder

---
+++ Praktikum-3/study-breeze/database/migrations/2024_09_29_130954_create_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('Product Name');
+            $table->string('Unit');
+            $table->string('Type');
+            $table->string('Informations');
+            $table->integer('qty');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};
